937a2991b428cb05f0a12303d412e8ff59e16a29
Jayson Gazeley <89006649+jgazeley@users.noreply.github.com>
2025-06-20 17:05:11 -0700

    Delete firmware/targets/slave_pic18f/build directory

--- firmware/targets/slave_pic18f/build/hal_pic18f.d
+++
@@ -1,2 +0,0 @@
-build\hal_pic18f.p1: hal/hal_pic18f.c hal/../../../common/hal/hal_gpio.h \
-  hal/../../../common/hal/hal_uart.h hal/config.h
--- firmware/targets/slave_pic18f/build/main.d
+++
@@ -1,2 +0,0 @@
-build\main.p1: app/main.c app/../hal/config.h \
-  app/../../../common/hal/hal_gpio.h
